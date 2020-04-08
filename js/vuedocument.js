7708e1b9388b523fa3605c2b7ab400894d87ca91
Almu <almu_atleticahotmail.com>
2020-04-08 12:31:08 +0100

    poniendo el toggle nav manu en pantallas pequeñas en vue.

--- js/vuedocument.js
+++ js/vuedocument.js
@@ -1,6 +1,21 @@
+
 var app = new Vue({
     el: '#app',
     data: {
       webName: "motobox",
-    }
+    },
+    methods: {
+      hideShow(){
+        const menu = document.querySelector('.nav_container');
+        if (menu.classList.contains('is-active')){
+          menu.classList.remove('is-active');
+        }
+        else{
+          menu.classList.add('is-active');
+        }
+
+      }
+    },
+
+
   })
